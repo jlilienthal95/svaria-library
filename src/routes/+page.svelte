9dee1c5929eca419dce0a8364bf8c7ec4ec48727
joggest95 <jilienthal@gmail.com>
2025-03-19 13:46:33 -0400

    removed metatags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 
 <script>
-    import { MetaTags } from 'svelte-meta-tags';
+    // import { MetaTags } from 'svelte-meta-tags';
     import Hero from "../FrontendComponents/HomePage/Main.svelte";
     import WhySvARIA from "../FrontendComponents/HomePage/WhySvARIA.svelte";
     import GettingStarted from "../FrontendComponents/HomePage/GettingStarted.svelte";
@@ -15,8 +15,8 @@
     <div></div>
 </main>
 
-<MetaTags
+<!-- <MetaTags
     title = 'Comprehensive Accessible Component Library and Toolkit'
     titleTemplate = '%s | Build Accessible UIs for Svelte'
     description = "SvARIA puts the power of building accessible UIs into the hands of any developer with an easy-to-use NPM package."
-/>
+/> -->
